3ac6f66a6aa10e6d54b83cec5c2fccb69c86bfe9
samirtimurkhanov <Samirik57@gmail.com>
2025-04-23 11:20:11 +0300

    Add files via upload

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -1,6 +1,6 @@
 'use strict'
 document.addEventListener("DOMContentLoaded", () => {
-    console.log('Скрипт отработал корректно')
+
 
     // 1.  Изменение состояния элемента при наведении, появление текста в элементе при наведении 
 
@@ -19,11 +19,44 @@ document.addEventListener("DOMContentLoaded", () => {
     //  4. Конец. 
     // БЛОК-СХЕМА: images/Block-schema.png.png
 
-    const intensiveImg = document.querySelector(".photo__photo");
-    intensiveImg.addEventListener('mouseenter', () => {
-        console.log('Мышка наведена на изображение, показываем текст');
-    });
+ 
 
+    // Объявляем переменную-массив intensiveImg и сохраняем в нее все элементы на странице с классом intensive__img  
+      const photoItem = document.querySelectorAll('.photo__item');
+      photoItem.forEach((item, index) => {
+        const photoText = document.querySelectorAll('.photo__name');
+        item.addEventListener('mouseenter', () => {
+         photoText[index].removeAttribute('hidden');
+    });
+        item.addEventListener('mouseleave', () => {
+            photoText[index].setAttribute('hidden', true);
+        });
+    });
 });
 
+// Получаем кнопку
+const scrollUpButton = document.querySelector(".scroll-up");
 
+// Показываем/скрываем кнопку при прокрутке
+if (scrollUpButton) {
+    const windowHeight = document.documentElement.clientHeight; // Определяем высоту видимой части окна браузера
+
+    // Показать кнопку при прокрутке вниз на высоту экрана
+    document.addEventListener('scroll', () => {
+        let scrollPageY = this.scrollY;
+
+        if (scrollPageY >= windowHeight) {
+            scrollUpButton.classList.add('scroll-up--show');
+        } else {
+            scrollUpButton.classList.remove('scroll-up--show');
+        }
+    });
+
+    // Плавная прокрутка наверх при нажатии на кнопку
+    scrollUpButton.addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    });
+}
